d9dca8528c6d86829ac83de6c68e0a56564cfe0e
moritzfelipe <stellmacher@gmail.com>
2024-11-26 19:06:44 +0100

    style: Update Footer component styling and layout
    - Changed footer background color from white to a deep blue (#00008B) for improved visual contrast.
    - Adjusted padding and margin for a more compact layout.
    - Updated text color to white and reduced text size for better readability.
    
    These changes enhance the visual appeal and user experience of the footer section.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,12 +1,10 @@
 export function Footer() {
   return (
-    <footer className="bg-white mt-auto">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="mt-8 md:mt-0">
-          <p className="text-center text-base text-gray-400">
-            &copy; 2024 Pionext
-          </p>
-        </div>
+    <footer className="bg-[#00008B] mt-auto">
+      <div className="container mx-auto py-4 px-8">
+        <p className="text-sm text-white">
+          &copy; 2024 Pionext
+        </p>
       </div>
     </footer>
   )
