f218e6891b5d88d412c844325d4433b8346baaba
Jonath-z <jonathanzihindula95@gmail.com>
2024-11-28 10:44:51 +0200

    chore: add ICP setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const staticBuildEnabled = process.env.STATIC_BUILD_ENABLED === "true";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  output: staticBuildEnabled ? "export" : undefined,
+};
 
 export default nextConfig;
